bc08e245f97935d5671164c02b37502c0a0ceeb9
drake <drake@innorix.com>
2025-09-03 21:07:24 +0700

    [fix] trim content new post blog

--- src/views/private/EditPost.jsx
+++ src/views/private/EditPost.jsx
@@ -1,9 +1,9 @@
 import React, {
-  useRef,
-  useState,
-  useEffect,
-  Suspense,
-  useContext,
+    useRef,
+    useState,
+    useEffect,
+    Suspense,
+    useContext,
 } from 'react';
 import styles from './EditPost.module.scss';
 import Editor from 'react-markdown-editor-lite';
@@ -21,141 +21,143 @@ import Cookies from 'js-cookie';
 import MainToast from '../../components/utils/toast/MainToast';
 
 const Footer = React.lazy(() =>
-  import('../../components/main-layout/footer/Footer')
+    import('../../components/main-layout/footer/Footer')
 );
 
 const EditPost = () => {
-  const mdEditor = useRef(null);
-  const titleRef = useRef(null);
+    const mdEditor = useRef(null);
+    const titleRef = useRef(null);
 
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [editStatus, setEditStatus] = useState({
-    isSuccess: false,
-    show: false,
-  });
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [editStatus, setEditStatus] = useState({
+        isSuccess: false,
+        show: false,
+    });
 
-  const { showModal, setIsValid } = useContext(BlogContext);
+    const { showModal, setIsValid } = useContext(BlogContext);
 
-  const LIMIT_TITLE_LENGTH = '190';
+    const LIMIT_TITLE_LENGTH = '190';
 
-  useEffect(() => {
-    document.title = title;
+    useEffect(() => {
+        document.title = title;
 
-    title && content ? setIsValid(true) : setIsValid(false);
-  }, [title, content, setIsValid]);
+        title && content ? setIsValid(true) : setIsValid(false);
+    }, [title, content, setIsValid]);
 
-  useEffect(() => {
-    const controller = new AbortController();
+    useEffect(() => {
+        const controller = new AbortController();
 
-    (async () => {
-      try {
-        const res = await fetch(`${apiURL}/blog/${location.pathname}`);
+        (async () => {
+            try {
+                const res = await fetch(`${apiURL}/blog/${location.pathname}`);
 
-        const data = await res.json();
+                const data = await res.json();
 
-        titleRef.current.innerText = data.blogSlug.title;
-        setTitle(data.blogSlug.title);
-        setContent(data.blogSlug.content);
+                titleRef.current.innerText = data.blogSlug.title;
+                setTitle(data.blogSlug.title);
+                setContent(data.blogSlug.content);
 
-        document.title = data.blogSlug.title;
-      } catch (error) {
-        console.log(error.message);
-      }
-    })();
+                document.title = data.blogSlug.title;
+            } catch (error) {
+                console.log(error.message);
+            }
+        })();
 
-    return () => controller?.abort();
-  }, [location.pathname]);
+        return () => controller?.abort();
+    }, [location.pathname]);
 
-  const blogData = async () => {
-    try {
-      const token = Cookies.get('token');
-      if (!token) return;
+    const blogData = async () => {
+        try {
+            const token = Cookies.get('token');
+            if (!token) return;
 
-      const res = await fetch(`${apiURL}/blog/${location.pathname}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          title: titleRef.current.innerText,
-          content: mdEditor.current.getMdValue(),
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      });
+            const res = await fetch(`${apiURL}/blog/${location.pathname}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    title: titleRef.current.innerText.trim(),
+                    content: mdEditor.current.getMdValue().trim(),
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-      const data = await res.json();
+            const data = await res.json();
 
-      const isEditBlogSuccess = data.success;
+            const isEditBlogSuccess = data.success;
 
-      if (isEditBlogSuccess) {
-        navigate(-1);
-        setEditStatus((prev) => {
-          return {
-            ...prev,
-            isSuccess: true,
-            show: true,
-          };
-        });
-      } else {
-        setEditStatus((prev) => {
-          return {
-            ...prev,
-            isSuccess: false,
-            show: true,
-          };
-        });
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const editorChange = ({ text }) => setContent(text);
-
-  return (
-    <>
-      {!showModal && (
-        <>
-          <Header blogData={blogData} />
-          <div className={styles.wrapper}>
-            <ContentEditable
-              text={'Tiêu đề'}
-              className={styles.contentEditable}
-              onInput={(e) => setTitle(e.target.innerText)}
-              maxLength={LIMIT_TITLE_LENGTH}
-              ref={titleRef}
-            />
-            <Editor
-              ref={mdEditor}
-              value={content}
-              onChange={editorChange}
-              renderHTML={(text) => <ReactMarkdown children={text} />}
-            />
-          </div>
-        </>
-      )}
-      <MainToast
-        status={editStatus}
-        setStatus={() =>
-          setEditStatus((prev) => {
-            return {
-              ...prev,
-              show: false,
-            };
-          })
+            if (isEditBlogSuccess) {
+                navigate(-1);
+                setEditStatus((prev) => {
+                    return {
+                        ...prev,
+                        isSuccess: true,
+                        show: true,
+                    };
+                });
+            } else {
+                setEditStatus((prev) => {
+                    return {
+                        ...prev,
+                        isSuccess: false,
+                        show: true,
+                    };
+                });
+            }
+        } catch (error) {
+            console.log(error.message);
         }
-        successText={'Chỉnh sửa bài viết thành công!'}
-        failText={'Chỉnh sửa bài viêt không thành công!'}
-      />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Footer />
-      </Suspense>
-    </>
-  );
+    };
+
+    const editorChange = ({ text }) => setContent(text);
+
+    return (
+        <>
+            {!showModal && (
+                <>
+                    <Header blogData={blogData} />
+                    <div className={styles.wrapper}>
+                        <ContentEditable
+                            text={'Tiêu đề'}
+                            className={styles.contentEditable}
+                            onInput={(e) => setTitle(e.target.innerText)}
+                            maxLength={LIMIT_TITLE_LENGTH}
+                            ref={titleRef}
+                        />
+                        <Editor
+                            ref={mdEditor}
+                            value={content}
+                            onChange={editorChange}
+                            renderHTML={(text) => (
+                                <ReactMarkdown children={text} />
+                            )}
+                        />
+                    </div>
+                </>
+            )}
+            <MainToast
+                status={editStatus}
+                setStatus={() =>
+                    setEditStatus((prev) => {
+                        return {
+                            ...prev,
+                            show: false,
+                        };
+                    })
+                }
+                successText={'Chỉnh sửa bài viết thành công!'}
+                failText={'Chỉnh sửa bài viêt không thành công!'}
+            />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Footer />
+            </Suspense>
+        </>
+    );
 };
 
 export default EditPost;
--- src/views/private/NewPost.jsx
+++ src/views/private/NewPost.jsx
@@ -1,99 +1,107 @@
-import React, { useRef, useState, useEffect, Suspense, useContext } from 'react'
-import styles from './NewPost.module.scss'
-import Editor from 'react-markdown-editor-lite'
-import ReactMarkdown from 'react-markdown'
-import 'react-markdown-editor-lite/lib/index.css'
-import '../../sass/_myIcon.scss'
-import Header from '../../components/main-layout/nav/Header'
-import '../../sass/_markdownEditor.scss'
-import ContentEditable from '../../components/utils/content-editable/ContentEditable'
-import Modal from '../../components/new-post/Modal'
-import { useLocation } from 'react-router-dom'
-import { apiURL } from '../../context/constants'
-import { BlogContext } from '../../context/BlogContext'
-import Cookies from 'js-cookie'
+import React, {
+    useRef,
+    useState,
+    useEffect,
+    Suspense,
+    useContext,
+} from 'react';
+import styles from './NewPost.module.scss';
+import Editor from 'react-markdown-editor-lite';
+import ReactMarkdown from 'react-markdown';
+import 'react-markdown-editor-lite/lib/index.css';
+import '../../sass/_myIcon.scss';
+import Header from '../../components/main-layout/nav/Header';
+import '../../sass/_markdownEditor.scss';
+import ContentEditable from '../../components/utils/content-editable/ContentEditable';
+import Modal from '../../components/new-post/Modal';
+import { useLocation } from 'react-router-dom';
+import { apiURL } from '../../context/constants';
+import { BlogContext } from '../../context/BlogContext';
+import Cookies from 'js-cookie';
 
 const Footer = React.lazy(() =>
-  import('../../components/main-layout/footer/Footer'),
-)
+    import('../../components/main-layout/footer/Footer')
+);
 
 const NewPost = () => {
-  const mdEditor = useRef(null)
-  const titleRef = useRef(null)
+    const mdEditor = useRef(null);
+    const titleRef = useRef(null);
 
-  const { showModal, setIsValid } = useContext(BlogContext)
+    const { showModal, setIsValid } = useContext(BlogContext);
 
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
-  useEffect(() => {
-    document.title = title ? title : 'Viết blog | F8'
+    useEffect(() => {
+        document.title = title ? title : 'Viết blog | F8';
 
-    title && content ? setIsValid(true) : setIsValid(false)
-  }, [title, content, setIsValid])
+        title && content ? setIsValid(true) : setIsValid(false);
+    }, [title, content, setIsValid]);
 
-  const LIMIT_TITLE_LENGTH = '190'
-  const blogData = () => {
-    if (
-      mdEditor.current &&
-      title.length > 0 &&
-      title.length <= LIMIT_TITLE_LENGTH
-    ) {
-      const data = {
-        title: titleRef.current.innerText,
-        content: mdEditor.current.getMdValue(),
-      }
-      console.log('blogData')
+    const LIMIT_TITLE_LENGTH = '190';
+    const blogData = () => {
+        if (
+            mdEditor.current &&
+            title.length > 0 &&
+            title.length <= LIMIT_TITLE_LENGTH
+        ) {
+            const data = {
+                title: titleRef.current.innerText.trim(),
+                content: mdEditor.current.getMdValue().trim(),
+            };
+            console.log('blogData');
 
-      titleRef.current.innerText = data.title
+            titleRef.current.innerText = data.title;
 
-      setTitle(data.title)
-      setContent(data.content)
-    }
-  }
+            setTitle(data.title);
+            setContent(data.content);
+        }
+    };
 
-  const readingTime = (content) => {
-    const WORDS_PER_MINUTE = 200 // People read 200 words/min https://infusion.media/content-marketing/how-to-calculate-reading-time/
-    const SMALLEST_READING_TIME = 1
+    const readingTime = (content) => {
+        const WORDS_PER_MINUTE = 200; // People read 200 words/min https://infusion.media/content-marketing/how-to-calculate-reading-time/
+        const SMALLEST_READING_TIME = 1;
 
-    const wordCount = content.split(' ').length
-    const minute = Math.floor(wordCount / WORDS_PER_MINUTE)
+        const wordCount = content.split(' ').length;
+        const minute = Math.floor(wordCount / WORDS_PER_MINUTE);
 
-    console.log('readingTime')
+        console.log('readingTime');
 
-    return minute <= SMALLEST_READING_TIME ? SMALLEST_READING_TIME : minute
-  }
+        return minute <= SMALLEST_READING_TIME ? SMALLEST_READING_TIME : minute;
+    };
 
-  const editorChange = ({ text }) => setContent(text)
+    const editorChange = ({ text }) => setContent(text);
 
-  return (
-    <>
-      {!showModal && (
+    return (
         <>
-          <Header blogData={blogData} />
-          <div className={styles.wrapper}>
-            <ContentEditable
-              text={'Tiêu đề'}
-              className={styles.contentEditable}
-              onInput={(e) => setTitle(e.target.innerText)}
-              maxLength={LIMIT_TITLE_LENGTH}
-              ref={titleRef}
-            />
-            <Editor
-              ref={mdEditor}
-              value={content}
-              onChange={editorChange}
-              renderHTML={(text) => <ReactMarkdown children={text} />}
-            />
-          </div>
+            {!showModal && (
+                <>
+                    <Header blogData={blogData} />
+                    <div className={styles.wrapper}>
+                        <ContentEditable
+                            text={'Tiêu đề'}
+                            className={styles.contentEditable}
+                            onInput={(e) => setTitle(e.target.innerText)}
+                            maxLength={LIMIT_TITLE_LENGTH}
+                            ref={titleRef}
+                        />
+                        <Editor
+                            ref={mdEditor}
+                            value={content}
+                            onChange={editorChange}
+                            renderHTML={(text) => (
+                                <ReactMarkdown children={text} />
+                            )}
+                        />
+                    </div>
+                </>
+            )}
+            {showModal && <Modal blogContent={{ title, content }} />}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Footer />
+            </Suspense>
         </>
-      )}
-      {showModal && <Modal blogContent={{ title, content }} />}
-      <Suspense fallback={<div>Loading...</div>}>
-        <Footer />
-      </Suspense>
-    </>
-  )
-}
+    );
+};
 
-export default NewPost
+export default NewPost;
