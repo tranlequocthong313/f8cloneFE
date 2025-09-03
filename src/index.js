0520aa37826f18e89f5891daa4aedb7d04cae120
drake <drake@innorix.com>
2025-09-03 10:31:34 +0700

    [build] remove console.log on prod

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
-import NavContextProvider from './context/NavContext'
-import { Provider } from 'react-redux'
-import store from './reducers/index'
-import LearningContextProvider from './context/LearningContext'
-import BlogContextProvider from './context/BlogContext'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.scss';
+import NavContextProvider from './context/NavContext';
+import { Provider } from 'react-redux';
+import store from './reducers/index';
+import LearningContextProvider from './context/LearningContext';
+import BlogContextProvider from './context/BlogContext';
+
+if (process.env.NODE_ENV === 'production') {
+    console.log = () => {};
+    console.error = () => {};
+    console.debug = () => {};
+}
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <NavContextProvider>
-        <LearningContextProvider>
-          <BlogContextProvider>
-            <App />
-          </BlogContextProvider>
-        </LearningContextProvider>
-      </NavContextProvider>
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root'),
-)
+    <BrowserRouter>
+        <Provider store={store}>
+            <NavContextProvider>
+                <LearningContextProvider>
+                    <BlogContextProvider>
+                        <App />
+                    </BlogContextProvider>
+                </LearningContextProvider>
+            </NavContextProvider>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
